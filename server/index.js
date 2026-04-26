ceb42be80bb19dc689e8b0b95b3b96e8956537a9
ezrakimathi66 <ezrakimathi66@gmail.com>
2026-04-26 14:12:41 +0300

    Fix CORS by hardcoding Vercel URL

--- server/index.js
+++ server/index.js
@@ -7,22 +7,14 @@ dotenv.config();
 
 const app = express();
 
+// CORS Configuration
 const corsOptions = {
-  origin: function(origin, callback) {
-    const allowedOrigins = process.env.NODE_ENV === 'production'
-      ? [process.env.CLIENT_URL || 'http://localhost:3000']
-      : ['http://localhost:3000', 'http://localhost:5173'];
-    
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('CORS not allowed'));
-    }
-  },
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://civic-mind-8bup.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
+
 app.use(cors(corsOptions));
 app.use(express.json());
 
